0b981f7f4fbabf8ba52df262cf6071571bb5ea72
MedAmineHachicha <45513508+MedAmineHachicha@users.noreply.github.com>
2018-12-01 14:12:30 +0100

    Add files via upload

---
+++ central.py
@@ -0,0 +1,41 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+import sklearn
+
+train_data=pd.read_csv('train.csv', delimiter=',')
+test_data=pd.read_csv('test.csv', delimiter=',')
+
+#changing Sex to binary
+train_data.Sex[train_data.Sex == 'male'] = 0
+train_data.Sex[train_data.Sex == 'female'] = 1
+
+#adding column for Title
+train_data['Title'] = train_data.Name.str.extract('([A-Za-z]+)\.', expand = True)
+
+#dataset description
+d = train_data.describe()
+
+#Filling missing values of Age
+means_by_title = train_data[['Title', 'Age']].groupby('Title', sort= False).mean()
+print(means_by_title)
+
+features=['PassengerId','Name','Age']
+
+temp = train_data[features]      # Copying ages data with Id
+nan_rows = temp[temp.isnull().any(axis=1)]
+print(nan_rows.loc[:, ['Age']])
+
+
+
+
+print(pd.crosstab(train_data['Title'], train_data['Sex']))
+print(pd.crosstab(train_data['Title'], train_data['Survived']))
+
+# calculating means
+
+
+
